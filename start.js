b8b9fb43d163ebcd87f5bb42d32469df716c56c4
Aayushi132 <112529819+Aayushi132@users.noreply.github.com>
2022-11-06 11:18:38 -0800

    first commit

--- start.js
+++ start.js
@@ -1,5 +1,23 @@
-const app = require('./app');
+require("dotenv").config();
 
-const server = app.listen(3000, function (){
-    console.log(`Express is running on port ${server.address().port}`);
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection
+  .on("open", () => {
+    console.log("Mongoose connection open");
+  })
+  .on("error", (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+require('./models/Registration');
+const app = require("./app");
+
+const server = app.listen(3000, function () {
+  console.log(`Express is running on port ${server.address().port}`);
 });
